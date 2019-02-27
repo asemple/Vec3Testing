b9543106233b13dc6a2aa10eaf437c996520c60d
asemple <s5007639@bournemouth.ac.uk>
2019-02-27 13:24:39 +0000

    added default ctor

--- Vec3Tests.cpp
+++ Vec3Tests.cpp
@@ -6,7 +6,19 @@ using namespace ::testing;
 // my lecture https://nccastaff.bournemouth.ac.uk/jmacey/PP/slides/testing/slides.html#/
 
 
-TEST(Vec3, fail)
+TEST(Vec3, testDefaultConstructor)
 {
-    ASSERT_EQ(1,1);
+    Vec3 a;
+    ASSERT_FLOAT_EQ(a.m_x, 0.0f);
+    ASSERT_FLOAT_EQ(a.m_y, 0.0f);
+    ASSERT_FLOAT_EQ(a.m_z, 0.0f);
+}
+
+
+TEST(Vec3, userDefinedConstructor)
+{
+    Vec3 a(1.0f, 2.0f, 3.0f);
+    ASSERT_FLOAT_EQ(a.m_x, 1.0f);
+    ASSERT_FLOAT_EQ(a.m_y, 2.0f);
+    ASSERT_FLOAT_EQ(a.m_z, 3.0f);
 }
